4c28fd522ab855307f8ea9241007ffa440dd8919
cespirit <ckespir@gmail.com>
2014-12-19 13:46:52 -0800

    Show message for invalid menu options

--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@ $(document).ready(function(){
 		                       ["Mom", "Blue", "Officer Jenny", "Professor Oak"], 
 		                       "Professor Oak");
 
-	/*
-	questions[1] = new Question("Who is the gym leader of Pewter City that favors Rock type pokemon? rjern erjner kfe krne knrek  kenkr enrk nwnej rntj ejrjeb", 
+	
+	questions[1] = new Question("Who is the gym leader of Pewter City that favors Rock type pokemon?", 
 		                       ["Giovanni", "Misty", "Brock", "Koga"], 
 		                       "Brock");
+	/*
 	questions[2] = new Question("What is the pokemon type of the infamous MissingNo. glitch?", 
-		                       ["Ghost", "Bird", "Psychic", "Fire"], 
+		                       ["Bird", "Ghost", "Psychic", "Fire"], 
 		                       "Bird");
 	*/		                  
 
@@ -18,18 +19,17 @@ $(document).ready(function(){
 	var actionQueue = ["enemyAttacks", "menu", "playerAttacks", "outcome"];
 	var actionIndex = 0;
 	var resultsQueue = ["enemyCondition", "expGained", "quizResults", "playAgain"];
-	var resultsIndex = 0;
-	var gameOver = false;
+	var resultsIndex = 0;	
 	var gameStart = true;
 	var userWins = false;
-	var score = 0;
-	var isDialogClickable = true;
+	var gameOver = false;
+	var score = 0;	
 	var enemyHP = 100;
 	var damagePerQuestion = Math.ceil(100/questions.length);
+	var isDialogClickable = true;
 	var currentQuestion;
 	var currentAnswer;
-	var userChoice;
-	
+	var userChoice;	
 
 	$(".dialog-section").click(function(){
 		if(gameStart) {
@@ -97,17 +97,16 @@ $(document).ready(function(){
 				case "quizResults":
 					setDialogText("<p><span class='stand-out'>Player</span> answered " + score + " out of " + questions.length + " correctly!</p>");
 					updateResultsIndex();
-					/*isDialogClickable = false;*/
 					break;
 				case "playAgain": 
 					setDialogText("Click to play again!</p>");
+					updateResultsIndex();
+					gameStart = true;
+					gameOver = false;					
+					userWins = false;
 					enemyHP = 100;
 					$(".enemy-section .hp-remaining").css("width", "100%");
-					score = 0;
-					gameOver = false;
-					gameStart = true;
-					updateResultsIndex();
-					userWins = false;
+					score = 0;										
 					break;
 			}
 			
@@ -116,13 +115,27 @@ $(document).ready(function(){
 
 
 	/* Extra for other menu options (optional) */
-	$("#dialog-menu a").click(function(event) {
+	$("#dialog-menu .invalid-option").click(function(event) {
 		event.preventDefault();
+		var option = $(this).attr('id');
+		switch(option) {
+			case "menu-item":
+				showMessage("<p>Your left your backpack at home!</p>");
+				break;
+			case "menu-pkmn":
+				showMessage("<p>You forgot your pokemon!</p>");
+				break;
+			case "menu-run":
+				showMessage("</p>You cannot escape!</p>");
+				break;
+		}
 	});
 
 
+	/* User chooses to answer question */
 	$("#dialog-menu #menu-answer").click(function(event){
 		event.preventDefault();
+		$("#message").hide();
 		$("#dialog-menu").hide();
 		$("#dialog-answer-options").empty();
 		currentQuestion = questions[questionIndex].qText;		
@@ -131,10 +144,10 @@ $(document).ready(function(){
 		$("#dialog-answer-options").show();		
 	});
 
-	
+	/* User selects an answer */
 	$("#dialog-answer-options").on("click", "a", function(event){
 		event.preventDefault();
-		userChoice = event.target.text;
+		userChoice = event.target.text;		
 		isDialogClickable = true;
 	});
 
@@ -168,6 +181,11 @@ $(document).ready(function(){
 
 	function setDialogText(text) {
 		$("#dialog-text .text").html(text);
+	}
+
+	function showMessage(msg) {
+		$("#message").show();
+		$("#message").html(msg);
 	}
 
 	function makeQuestionOptions() {
