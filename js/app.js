73f2a2f9306f728ed4df4230382d93c4bac1c2c8
cespirit <ckespir@gmail.com>
2014-12-19 12:54:51 -0800

    Quiz allows for play again

--- js/app.js
+++ js/app.js
@@ -4,19 +4,23 @@ $(document).ready(function(){
 	questions[0] = new Question("Who gives you your first pokemon?", 
 		                       ["Mom", "Blue", "Officer Jenny", "Professor Oak"], 
 		                       "Professor Oak");
+
+	/*
 	questions[1] = new Question("Who is the gym leader of Pewter City that favors Rock type pokemon? rjern erjner kfe krne knrek  kenkr enrk nwnej rntj ejrjeb", 
 		                       ["Giovanni", "Misty", "Brock", "Koga"], 
 		                       "Brock");
 	questions[2] = new Question("What is the pokemon type of the infamous MissingNo. glitch?", 
 		                       ["Ghost", "Bird", "Psychic", "Fire"], 
 		                       "Bird");
+	*/		                  
 
 	var questionIndex = 0;
 	var actionQueue = ["enemyAttacks", "menu", "playerAttacks", "outcome"];
 	var actionIndex = 0;
-	var resultsQueue = ["enemyCondition", "expGained", "quizResults"];
+	var resultsQueue = ["enemyCondition", "expGained", "quizResults", "playAgain"];
 	var resultsIndex = 0;
 	var gameOver = false;
+	var gameStart = true;
 	var userWins = false;
 	var score = 0;
 	var isDialogClickable = true;
@@ -28,7 +32,12 @@ $(document).ready(function(){
 	
 
 	$(".dialog-section").click(function(){
-		if(!gameOver && isDialogClickable) {
+		if(gameStart) {
+			$("#enemy-sprite i").removeClass("fainted");
+			$("#enemy-stats").removeClass("invisible");	
+			setDialogText("<p class='text'>Wild <span class='stand-out'>Quiz</span> appeared!</p>");
+			gameStart = false;
+		} else if(!gameOver && isDialogClickable) {
 			switch(actionQueue[actionIndex]) {
 				case "enemyAttacks":
 					setDialogText("<p>Enemy <span class='stand-out'>Quiz</span> Used <span class='stand-out'>Question!</span></p>");
@@ -44,12 +53,12 @@ $(document).ready(function(){
 				case "playerAttacks":
 					currentAnswer = questions[questionIndex].qAnswer;					
 					$("#dialog-answer-options").hide();
-					setDialogText("<p><span class='stand-out'>Player</span> answers with <span class='stand-out'>" + userChoice + "</span>!</p>");	
-					$("#enemy-sprite i").effect("shake");										
+					setDialogText("<p><span class='stand-out'>Player</span> answers with <span class='stand-out'>" + userChoice + "</span>!</p>");										
 					updateActionIndex();
 					break;
 				case "outcome":
 					if(userChoice === currentAnswer) {
+						$("#enemy-sprite i").effect("shake");	
 						setDialogText("<p>It's super effective!</p>");
 						calcEnemyHP();
 						score++;
@@ -68,7 +77,9 @@ $(document).ready(function(){
 		} else if(gameOver && isDialogClickable) {
 			switch(resultsQueue[resultsIndex]) {
 				case "enemyCondition":
-					if(userWins) {
+					$("#enemy-sprite i").addClass("fainted");
+					$("#enemy-stats").addClass("invisible");					
+					if(userWins) {						
 						setDialogText("<p>Enemy <span class='stand-out'>Quiz</span> fainted!</p>");
 					} else {
 						setDialogText("<p>Enemy <span class='stand-out'>Quiz</span> ran away!</p>");
@@ -86,7 +97,17 @@ $(document).ready(function(){
 				case "quizResults":
 					setDialogText("<p><span class='stand-out'>Player</span> answered " + score + " out of " + questions.length + " correctly!</p>");
 					updateResultsIndex();
-					isDialogClickable = false;
+					/*isDialogClickable = false;*/
+					break;
+				case "playAgain": 
+					setDialogText("Click to play again!</p>");
+					enemyHP = 100;
+					$(".enemy-section .hp-remaining").css("width", "100%");
+					score = 0;
+					gameOver = false;
+					gameStart = true;
+					updateResultsIndex();
+					userWins = false;
 					break;
 			}
 			
@@ -140,7 +161,7 @@ $(document).ready(function(){
 
 	function updateResultsIndex() {
 		resultsIndex++;
-		if(resultsIndex >= resultsIndex.length) {
+		if(resultsIndex >= resultsQueue.length) {
 			resultsIndex = 0;
 		}
 	}
